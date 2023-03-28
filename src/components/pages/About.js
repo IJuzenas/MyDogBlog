634de32461157b794f79f0e054716704c3293925
IJuzenas <juzenasignas@gmail.com>
2023-03-28 20:42:19 +0300

    Corrected About you page with picture and description

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,12 +3,17 @@ import loginpic from "../pages/pics/login.jpg";
 
 const About = () => (
     <>
-
-        <img src={"src/components/pages/pics/login.jpg"}
-             width="150vw"
-             height="150vh"
+        <h1>So, what you want to know about us?</h1>
+        <img src={loginpic}
+             width="800"
+             height="500"
+             align="center"
              alt={"So, what you want to know about us?"}>
         </img>
+        <p>
+            This is a dog blog for people to connect on what food is the best for their best friends with tails and to have a
+            great time exchanging the stories of our 4 legged buddies
+        </p>
     </>
 
 )
